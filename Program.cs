fb59eb3f75aa5f7d4974ce6c46168b5b7a444780
arturdiel <arturdanieldiel@gmail.com>
2025-08-23 23:48:39 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+class Program
+{
+    // Inversão de pilha usando 1 fila auxiliar
+    static void InverterPilha<T>(Stack<T> pilha)
+    {
+        Queue<T> fila = new Queue<T>();
+
+        // Desimpilhar e Enfileirar
+        while (pilha.Count > 0)
+        {
+            fila.Enqueue(pilha.Pop());
+        }
+
+        // Desinfileirar e Empilhar
+        while (fila.Count > 0)
+        {
+            pilha.Push(fila.Dequeue());
+        }
+
+    }
+
+    // Imprimir a pilha do topo para a base
+    static void Imprimir<T>(Stack<T> pilha)
+    {
+        foreach (var item in pilha)
+        {
+            Console.WriteLine($"{item}");
+        }
+    }
+
+    static void Main()
+    {
+        // Declara a pilha com os elementos
+        Stack<string> pilha = new Stack<string>(new[] { "D", "C", "B", "A" });
+
+        // Imrpime a pilha
+        Console.WriteLine("Pilha original:");
+        Imprimir(pilha);
+
+        // Inverte a pilha utilizando o metodo da classe
+        InverterPilha(pilha);
+
+        // Imprime a pilha invertida
+        Console.WriteLine("\nPilha invertida");
+        Imprimir(pilha);
+
+    }
+}
